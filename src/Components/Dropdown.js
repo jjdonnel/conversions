fdd2aca128c7d4cb1c2ba977a6bd473273eb582f
jjdonnel <jdonnelly71@yahoo.com>
2023-03-01 08:35:28 -0500

    touchend

--- src/Components/Dropdown.js
+++ src/Components/Dropdown.js
@@ -1,20 +1,13 @@
 import React from "react";
 import { useState, useRef, useEffect } from "react";
 import Arrow from "./Arrow";
-// import "./Navbar.css";
 import { Link } from "react-router-dom";
-// import { options } from "../items";
-
-function Dropdown({items, setMenuOpen, setSelected}) {
+function Dropdown({ items, setMenuOpen, setSelected }) {
   const [isToggled, setIsToggled] = useState(false);
   const ref = useRef();
-  const test = ()=> {
-    console.log(isToggled);
-  }
 
   const toggleSubMenu = () => {
     setIsToggled(!isToggled);
-    // console.log(isToggled);
   };
 
   const closeSubMenu = () => {
@@ -24,38 +17,13 @@ function Dropdown({items, setMenuOpen, setSelected}) {
 
   const closeSlideMenu = () => {
     setMenuOpen(false);
-  }
+  };
 
   const changeTitle = () => {
-    items.title == 'Home' ? setSelected('Conversions Etc') : setSelected(items.title);
-  }
-  // useEffect( () => { setHeight(document.documentElement.clientHeight) });
-  // useEffect(()=> {
-  //   const setClick = (event) => {
-  //     if (
-  //       isToggled &&
-  //       titleRef.current &&
-  //       titleRef.current.contains(event.target)
-  //     ) {
-  //       setIsToggled(false);
-  //     };
-      
-  //   }
-  //   document.addEventListener('mouseup', setClick);
-  //   return ()=> {
-  //     document.removeEventListener('mouseup', setClick);
-  //   }
-  // },[isToggled]);
-  
-  // const setClick = (event) => {
-  //     if (
-  //       isToggled &&
-  //       titleRef.current &&
-  //       titleRef.current.contains(event.target)
-  //     ) {
-  //       setIsToggled(false);
-  //     };
-  //   };
+    items.title === "Home"
+      ? setSelected("Conversions Etc")
+      : setSelected(items.title);
+  };
 
   const onMouseEnter = () => {
     setIsToggled(true);
@@ -65,66 +33,62 @@ function Dropdown({items, setMenuOpen, setSelected}) {
     setIsToggled(false);
   };
 
-    useEffect(() => {
+  useEffect(() => {
     const itemHandler = (event) => {
-      if (
-        isToggled &&
-        ref.current &&
-        !ref.current.contains(event.target)
-      ) {
+      if (isToggled && ref.current && !ref.current.contains(event.target)) {
         setIsToggled(false);
       }
     };
 
-    document.addEventListener('mousedown', itemHandler);
-    // document.addEventListener('touchstart', itemHandler);
+    document.addEventListener("mousedown", itemHandler);
+    document.addEventListener('touchend', itemHandler);
     return () => {
       // Cleanup the event listener
-      document.removeEventListener('mousedown', itemHandler);
-      // document.removeEventListener('touchstart', itemHandler);
+      document.removeEventListener("mousedown", itemHandler);
+      document.removeEventListener('touchend', itemHandler);
     };
-
   }, [isToggled]);
 
   return (
-
-
-          // !items.children  ? 
-          //   <li>
-          //     <div>
-          //       <Link to={items.path}>{items.title}</Link>
-          //     </div>
-          //   </li>
-          // :
-
-
-<li className="item" ref={ref} 
-onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}
->
-    {!items.children ? 
-    (
-      <div className="title"  onClick={()=>{closeSlideMenu(); changeTitle(); closeSubMenu()}} >
-        <Link to={items.path} >{items.title}</Link>
-      </div>
-    ) :
-    (
-      <div className="drop">
-        <div className="title"
-         onClick={toggleSubMenu} 
-         >
-          <span>{items.title}</span>
-          <div className="arrow">
-            <Arrow className={isToggled ? "arrow" : "arrow up"} width={15} />
-          </div>
+    <li
+      className="item"
+      ref={ref}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+    >
+      {!items.children ? (
+        <div
+          className="title"
+          onClick={() => {
+            closeSlideMenu();
+            changeTitle();
+            closeSubMenu();
+          }}
+        >
+          <Link to={items.path}>{items.title}</Link>
         </div>
-        <ul className={isToggled ? "submenu" : "submenu closed"}>
-          {items.children.map((child, index) => (
-            <Dropdown items={child} key={index} setMenuOpen={setMenuOpen} setSelected={setSelected} setIsToggled={setIsToggled} />
-          ))}
-        </ul>
-      </div>
-    )}
-  </li>
+      ) : (
+        <div className="drop">
+          <div className="title" onClick={toggleSubMenu}>
+            <span>{items.title}</span>
+            <div className="arrow">
+              <Arrow className={isToggled ? "arrow" : "arrow up"} width={15} />
+            </div>
+          </div>
+          <ul className={isToggled ? "submenu" : "submenu closed"}>
+            {items.children.map((child, index) => (
+              <Dropdown
+                items={child}
+                key={index}
+                setMenuOpen={setMenuOpen}
+                setSelected={setSelected}
+                setIsToggled={setIsToggled}
+              />
+            ))}
+          </ul>
+        </div>
+      )}
+    </li>
   );
 }
 
--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -1,177 +1,80 @@
-import { useState, useRef } from 'react';
+import { useState } from "react";
 import { Outlet, Link } from "react-router-dom";
-import './navbar.css';
-import Arrow from './Arrow';
-import { geoOptions, metOptions } from '../options';
-import { options, altOptions} from '../items';
-import Dropdown from './Dropdown';
+import "./navbar.css";
+import { options, altOptions } from "../items";
+import Dropdown from "./Dropdown";
 
-function Layout()  {
-  // const [ selected, setSelected ] = useState('Home');
-  const [ isToggled, setIsToggled ] = useState(false);
-  const [ geometry, setGeometry ] = useState(false);
-  const [ meteorology, setMeteorology ] = useState(false);
-  const [shapes, setShapes] = useState(false);
-  const parentRef = useRef();
-  const childRef = useRef();
-
-  const [ menuOpen, setMenuOpen ] = useState(false);
-  const [ selected, setSelected ] = useState('Conversions Etc');
+function Layout() {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [selected, setSelected] = useState("Conversions Etc");
 
   const closeSlideMenu = () => {
     setMenuOpen(false);
-  }
-    
-  return(
-      <div className="App">
+  };
 
-<div className='nav' >
-        <div className="menu" 
-        onClick={()=>{setMenuOpen(!menuOpen)}}
+  return (
+    <div className="App">
+      <div className="nav">
+        <div
+          className="menu"
+          onClick={() => {
+            setMenuOpen(!menuOpen);
+          }}
         >
-              <div className='dynamic'>
-                  <span>{selected}</span>
-              </div>
-              <div className='hamburger'>
-                  <span className={ menuOpen ? "line line1-open" : "line line1-closed" }></span>
-                  <span className={ menuOpen ? "line line2-open" : "line line2-closed" }></span>
-                  <span className={ menuOpen ? "line line3-open" : "line line3-closed" }></span>
-              </div>
+          <div className="dynamic">
+            <span>{selected}</span>
           </div>
-        {/* <MenuBtn /> */}
-        <ul className={ menuOpen? 'top top-open' : 'top top-closed'}>
-          {/* <div className='dynamic'>
-                  <span>{selected}</span>
-              </div> */}
-
-          {
-          window.innerWidth < 800 && altOptions ? 
-            <li className='mobileList'>
-              {altOptions.map((option, index)=>
-              <div className='mobile' style={ option.title == 'Home' ? {margin: '1.5em', fontSize: '1.25em', fontWeight: 'bold'} : {margin: '1.5em'}} key={index} onClick={closeSlideMenu}>
-                <Link to={option.path}>{option.title}</Link>
-              </div>)}
-            </li>
-          : !window.innerWidth < 800 &&
-          options.map((option, index) => 
-            <Dropdown items={option} key={index} setMenuOpen={setMenuOpen} setSelected={setSelected}/>
+          <div className="hamburger">
+            <span
+              className={menuOpen ? "line line1-open" : "line line1-closed"}
+            ></span>
+            <span
+              className={menuOpen ? "line line2-open" : "line line2-closed"}
+            ></span>
+            <span
+              className={menuOpen ? "line line3-open" : "line line3-closed"}
+            ></span>
+          </div>
+        </div>
+        <ul className={menuOpen ? "top top-open" : "top top-closed"}>
+          {(
+          // window.innerWidth < 800 && altOptions ? (
+          //   <li className="mobileList">
+          //     {altOptions.map((option, index) => (
+          //       <div
+          //         className="mobile"
+          //         style={
+          //           option.title === "Home"
+          //             ? {
+          //                 margin: "1.5em",
+          //                 fontSize: "1.25em",
+          //                 fontWeight: "bold",
+          //               }
+          //             : { margin: "1.5em" }
+          //         }
+          //         key={index}
+          //         onClick={closeSlideMenu}
+          //       >
+          //         <Link to={option.path}>{option.title}</Link>
+          //       </div>
+          //     ))}
+          //   </li>
+          // ) : (
+          //   !window.innerWidth < 800 &&
+            options.map((option, index) => (
+              <Dropdown
+                items={option}
+                key={index}
+                setMenuOpen={setMenuOpen}
+                setSelected={setSelected}
+              />
+            ))
           )}
-
-          
         </ul>
       </div>
-
-          {/* <nav className="nav">
-            <div className="menu" onClick={(e)=> {setIsToggled(!isToggled)}}>
-              <div className='dynamic'>
-                <span>{ selected }</span>
-                  <div className='arrow'>
-                      <Arrow className={isToggled ? 'top_arrow_icon' : 'top_arrow_icon top_rotate'} width={15}/>
-                  </div>
-              </div>
-              <div className='hamburger'>
-                <span className={ isToggled ? "line line1-open" : "line line1-closed" }></span>
-                <span className={ isToggled ? "line line2-open" : "line line2-closed" }></span>
-                <span className={ isToggled ? "line line3-open" : "line line3-closed" }></span>
-              </div>
-            </div>
-            <ul className={isToggled ? "top top-open" : "top top-closed"}>
-              <li className="drop" onClick={(e)=> {setSelected('Home'); setIsToggled(!isToggled);setGeometry(false);setMeteorology(false)}}>
-                <div className="title">
-                  <Link to="/">Home</Link>
-                </div>
-              </li>
-              <li className="drop">
-                <div className="title" onClick={(e)=> {setGeometry(!geometry);setMeteorology(false);setShapes(false)}}>
-                    <span>Geometry</span>
-                    <div className='arrow'>
-                        <Arrow className={geometry ? 'arrow_icon' : 'arrow_icon rotate'} width={15}/>
-                    </div>
-                </div>
-                
-                <ul ref={parentRef} className="submenu" style={geometry ? 
-                  {height: 'auto', opacity: '1', transition: 'all 0.2s ease'} :
-                  {height: '0px', opacity: '0', transition: '0.2s ease'}}>
-                  { geoOptions.map((option, index) => {
-                    if (option.children) {
-                      return (
-                        <li key={index} className="drop" >
-                          <Link to={option.path}></Link>
-                          <div className="title" onClick={(e) => { setShapes(!shapes); }}>
-                            {option.title}
-                            <div className="arrow">
-                              <Arrow className={ shapes ? "arrow_icon" : "arrow_icon rotate" } width={15} />
-                            </div>
-                          </div>
-
-                          <ul
-                            ref={childRef}
-                            className="childMenu"
-                            style={
-                              shapes
-                                ? {
-                                    height: "auto",
-                                    opacity: 1,
-                                    transition: "all 0.2s ease",
-                                  }
-                                : {
-                                    height: "0px",
-                                    opacity: 0,
-                                    transition: "0.2s ease",
-                                  }
-                            }
-                          >
-                            {option.children.map((child, index) => {
-                              return (
-                                <li key={index} onClick={
-                                (e) => { setSelected(option.title); setIsToggled(!isToggled); setMeteorology(false); setGeometry(false)}}>
-                                  <Link to={child.path}>{child.title}</Link>
-                                </li>
-                              );
-                            })}
-                          </ul>
-                        </li>
-                      );
-                    } else {
-                      return (
-                        <li key={index} onClick={
-                          (e) => { setSelected(option.title); setIsToggled(!isToggled); setMeteorology(false); setGeometry(false)}}>
-                          <Link to={option.path}>{option.title}</Link>
-                        </li>
-                      );
-                    }
-                    }
-                  )}
-                </ul>
-              </li>
-              <li className="drop">
-                <div className="title" onClick={(e)=> {setMeteorology(!meteorology);setGeometry(false);setShapes(false)}}>
-                    <span>Meteorology</span>
-                    <div className='arrow'>
-                        <Arrow className={meteorology ? 'arrow_icon' : 'arrow_icon rotate'} width={15}/>
-                    </div>
-                </div>
-                
-                <ul ref={parentRef} className="submenu" style={meteorology ? 
-                  {height: 'auto', opacity: '1', transition: '0.2s ease'} :
-                  {height: '0px', opacity: '0', transition: '0.2s ease'}}>
-                    { metOptions.map((option, index) => {
-                      return (
-                        <li key={index} onClick={
-                          (e) => { setSelected(option.title); setIsToggled(!isToggled); setMeteorology(false); setGeometry(false)}}>
-                            <Link to={option.path}>{option.title}</Link>
-                        </li>
-                      )
-                    })
-                    }
-                </ul>
-              </li>
-            </ul>
-          </nav> */}
-
       <Outlet />
-    </div> 
-
-  )}
+    </div>
+  );
+}
 
 export default Layout;
